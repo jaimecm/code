af367248ceb155452874a80b7ecb2fdc8d54dcf3
Jecina <jaime19802@gmail.com>
2024-11-07 20:42:02 -0500

    nuevo archivo

---
+++ Main.java
@@ -0,0 +1,41 @@
+package com.example;
+
+public class Main {
+  /*
+   * ver en token para ver error de escritura
+   * puclic void bucles(){
+   */
+  public void bucles() {
+    /*
+     * int number = 6;
+     * for (int i = 0; i < 3; i++) {
+     * // equivalentes
+     * number = number + number;
+     * number += number;
+     * System.out.println(number);
+     * }
+     */
+    /*
+     * String[] names = { "ARTURO", "PEDRO", "MIGUEL" };
+     * //da un error
+     * for (int i = 0; i <= names.length; i++) {
+     * System.out.println("los nombres son: " + names[i]);
+     * }
+     * 
+     * boolean IsRaining = false;
+     * while (IsRaining) {
+     * System.out.println("Lloviendo");
+     * }
+     */
+    double vehiculeSpeed = 0;
+    double speedLimit = 100;
+    while (vehiculeSpeed < speedLimit) {
+
+    }
+  }
+
+  public static void main(String[] args) {
+    Main foo = new Main();
+    foo.bucles();
+  }
+}
